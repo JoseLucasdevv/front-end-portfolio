76adb68cc3ec7f30dc05e38306b0d3bba1095f85
JoseLucasdevv <joselucas.dev@gmail.com>
2023-11-21 11:34:37 -0300

    feat:routers project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProjetsComponent } from './components/projets/projets.component';
+import { MainComponent } from './components/main/main.component';
+
+const routes: Routes = [
+  { path: '', component: MainComponent },
+
+  {
+    path: '**',
+    redirectTo: '/',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}

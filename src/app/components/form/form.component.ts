8519a6ac548b5c80ed7212cf8870cd70fd131f86
JoseLucasdevv <joselucas.dev@gmail.com>
2023-11-27 11:05:44 -0300

    feat:add link mailto

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss'],
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
+  form = new FormGroup({
+    formName: new FormControl(''),
+    formEmail: new FormControl(''),
+    formMessage: new FormControl(''),
+  });
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  onSubmit(): boolean | void {
+    if (!this.form.value.formEmail) return false;
+    console.log(this.form.value);
+  }
+
   goToUrl(url: string) {
     window.open(url);
   }

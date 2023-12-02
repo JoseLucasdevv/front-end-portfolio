f9f46eade8e29abfe642d5dde5ae751d026e8a4e
JoseLucasdevv <joselucas.dev@gmail.com>
2023-12-02 08:28:23 -0300

    feat:"add msg errors object"

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,29 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormGroup,
-  FormControl,
-  FormBuilder,
-  Validators,
-} from '@angular/forms';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { FormService } from 'src/app/services/form.service';
+import { Errors } from 'src/app/types/Errors-interface';
+import { ContactForm } from 'src/app/types/form-interface';
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
-  form = new FormGroup({
-    formName: new FormControl(''),
-    formEmail: new FormControl(''),
-    formMessage: new FormControl(''),
-  });
+  form!: FormGroup;
+  errorsMsg: Errors = {
+    msgEmail: 'Enter a Valid Email',
+    msgMinLength: 'The minimum number of characters is 6',
+    msgMaxLength: 'Maximum characters is 400',
+    msgRequired: 'Input cannot be empty',
+  };
 
-  constructor(private _formBuilder: FormBuilder) {}
+  valuesForm!: ContactForm;
 
-  ngOnInit(): void {}
+  constructor(
+    private _formService: FormService,
+    private _formBuilder: FormBuilder
+  ) {}
 
-  onSubmit(): boolean | void {
-    if (!this.form.value.formEmail) return false;
-    console.log(this.form.value);
+  ngOnInit(): void {
+    this.form = this._formBuilder.group({
+      formName: ['', Validators.required],
+      formEmail: ['', [Validators.required, Validators.email]],
+      formMessage: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(400),
+        ],
+      ],
+    });
+  }
+
+  onSubmit(): void | boolean {
+    if (this.form.status === 'INVALID') return false;
+
+    this._formService.send(this.form.value);
+
+    alert('send e-mail success');
+
+    this.form.reset();
   }
 
   goToUrl(url: string) {

ef118b0e2956ce224656fd15331d61b0aad76260
JoseLucasdevv <joselucas.dev@gmail.com>
2023-12-05 09:26:54 -0300

    feat:add navigate.ts for navigate links

---
+++ src/app/navigate.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Navigate {
+  constructor(private _router: Router) {}
+  scroll(el: string): void {
+    if (document.getElementById(el)) {
+      document.getElementById(el)?.scrollIntoView({ behavior: 'smooth' });
+    } else {
+      this._router
+        .navigate(['/home'])
+        .then(() =>
+          document.getElementById(el)?.scrollIntoView({ behavior: 'smooth' })
+        );
+    }
+  }
+}

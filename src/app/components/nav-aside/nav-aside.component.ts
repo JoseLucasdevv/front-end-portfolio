e44cd3fe209064a138c9430049ba711b33431cb1
JoseLucasdevv <joselucas.dev@gmail.com>
2023-12-13 18:18:17 -0300

    feat:add support languages English and Portuguese

--- src/app/components/nav-aside/nav-aside.component.ts
+++ src/app/components/nav-aside/nav-aside.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Navigate } from '../../navigate';
 import { BehaviorSubject } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-nav-aside',
   templateUrl: './nav-aside.component.html',
@@ -10,7 +11,10 @@ export class NavAsideComponent implements OnInit, OnDestroy {
   @Input() flag!: BehaviorSubject<boolean>;
   aside!: HTMLElement | null;
   html: HTMLElement = document.getElementsByTagName('html')[0];
-  constructor(private _navigate: Navigate) {}
+  constructor(
+    private _navigate: Navigate,
+    private _translate: TranslateService
+  ) {}
 
   ngOnInit(): void {
     this.html.style.overflowY = 'hidden';
@@ -35,6 +39,10 @@ export class NavAsideComponent implements OnInit, OnDestroy {
 
   scroll(el: string) {
     this._navigate.scroll(el);
+    this.flag.next(false);
+  }
+  switchLanguage(language: string) {
+    this._translate.use(language);
     this.flag.next(false);
   }
 }

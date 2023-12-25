8568a85497b426380f1ea3eb2d97a556b3635306
JoseLucasdevv <joselucas.dev@gmail.com>
2023-12-25 09:44:58 -0300

    refactor:change url back end in mode development

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiKey: 'https://api.github.com/users/JoseLucasdevv/repos',
   deployUrl: '-bice.vercel.app/',
-  urlBackEnd: 'https://send-email-service.onrender.com',
+  urlBackEnd: 'http://localhost:3000',
 };
